852a40e20d152f4666361a375aeac4f0258ce57f
dantheprofessional <danielgubermanemail@gmail.com>
2024-10-11 09:10:04 -0400

    changes flask code for restaurant searching

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+	query = request.form['query']
+	# Call a restaurant API
+	# For example, using Google Places API:
+	# response = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=your_location&radius=1500&type=restaurant&keyword={query}&key=YOUR_API_KEY')
+	# results = response.json()['results']
+	return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
